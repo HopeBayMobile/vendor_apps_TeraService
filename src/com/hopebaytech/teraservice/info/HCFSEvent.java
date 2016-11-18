f90e259549f7bddf96d81ae80f65fb992fef7aaf
Aaron <aaron.chu@happygorgi.com>
2016-11-18 16:27:38 +0800

    Broadcast intent when receiving booster process successful/failed event

--- src/com/hopebaytech/teraservice/info/HCFSEvent.java
+++ src/com/hopebaytech/teraservice/info/HCFSEvent.java
@@ -10,10 +10,13 @@ public class HCFSEvent {
     public static final int UPLOAD_COMPLETED = 2;
     public static final int RESTORE_STAGE_1 = 3;
     public static final int RESTORE_STAGE_2 = 4;
+    public static final int CREATE_THUMBNAIL = 7;
+    public static final int BOOSTER_PROCESS_COMPLETED = 8;
+    public static final int BOOSTER_PROCESS_FAILED = 9;
 
     // The following event is not implemented.
     public static final int EXCEED_PIN_MAX = 5;
     public static final int SPACE_NOT_ENOUGH = 6;
 
-    public static final int CREATE_THUMBNAIL = 7;
+
 }
--- src/com/hopebaytech/teraservice/info/TeraIntent.java
+++ src/com/hopebaytech/teraservice/info/TeraIntent.java
@@ -12,6 +12,8 @@ public class TeraIntent {
     public static final String ACTION_RESTORE_STAGE_1 = "hbt.intent.action.RESTORE_STAGE_1";
     public static final String ACTION_RESTORE_STAGE_2 = "hbt.intent.action.RESTORE_STAGE_2";
     public static final String ACTION_EXCEED_PIN_MAX = "hbt.intent.action.EXCEED_PIN_MAX";
+    public static final String ACTION_BOOSTER_PROCESS_COMPLETED = "hbt.intent.action.BOOSTER_PROCESS_COMPLETED";
+    public static final String ACTION_BOOSTER_PROCESS_FAILED = "hbt.intent.action.BOOSTER_PROCESS_FAILED";
 
     // Intent 
     public static final String KEY_RESTORE_ERROR_CODE = "intent_key_restore_error_code";
